816da2105cc299e9cf848260f0e866b371277981
PranetSolkar123 <pranet.2406927105@vcet.edu.in>
2026-06-06 16:38:41 +0530

    Music Player Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,123 @@
 import React from 'react'
+import Gramophone from './components/NewGramophone/Gramophone'
+import VinylDisc from './components/Vinyl/VinylDisc'
+import './index.css'
+import {useState, useRef} from 'react'
+import {songs} from './data/songs.js'
 
 const App = () => {
+  const [currentSong, setCurrentSong] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio());
+
+  //brain of website
+  function handleSongClick(song) {
+    setCurrentSong(song);
+
+    audioRef.current.src = song.audio;
+    audioRef.current.play();
+
+    setIsPlaying(true);
+
+
+
+  }
+
+  function toggleMusic() {
+    if(isPlaying){
+      audioRef.current.pause();
+      setIsPlaying(false);
+    }
+    else{
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+
+  }
+
+  function previousSong(){
+    if (!currentSong) return;
+
+    const currentIndex = songs.findIndex(
+      songs => songs.id === currentSong.id
+    );
+
+    const previousIndex = (currentIndex - 1 + songs.length) % songs.length;
+
+    handleSongClick(songs[previousIndex]);
+  }
+
+  function nextSong(){
+    if (!currentSong) return;
+
+    const currentIndex = songs.findIndex(
+      songs => songs.id === currentSong.id
+    );
+
+    const nextIndex = (currentIndex + 1) % songs.length;
+
+    handleSongClick(songs[nextIndex]);
+  }
+
+
+
+
+
   return (
-    <div>
+    // <div>
+    //   {
+    //     songs.map((song)=>(
+        
+          
+    //         // <button
+    //         // key={song.id}
+    //         // onClick={()=>handleSongClick(song)}
+    //         // >
+    //         //   {song.title}
+    //         // </button>
+    //         <VinylDisc key={song.id} song={song} onClick={()=>handleSongClick(song)} />
+           
+    //     ))
+    //   }
+
+    //   <h2>
+    //     Current Song: {currentSong?.title}
+    //   </h2>
+    //   <h2>
+    //     Status: {isPlaying ? "Playing" : "Paused"}
+    //   </h2>
+    //   <button onClick={previousSong}>Previous</button>
+    //   <button onClick={toggleMusic}>{isPlaying ? "Pause" : "Play"}</button>
+    //   <button onClick={nextSong}>Next</button>
+    // </div>
+    
+
+
+    <div className='mainContainer'>
+      <div className='songContainer1'>
+        {
+          songs.map((song)=>(
+          
+            
+              // <button
+              // key={song.id}
+              // onClick={()=>handleSongClick(song)}
+              // >
+              //   {song.title}
+              // </button>
+              <VinylDisc key={song.id} song={song} onClick={()=>handleSongClick(song)} />
+            
+          ))
+        }
+        {/* <VinylDisc/>
+        <VinylDisc/>
+        <VinylDisc/>
+        <VinylDisc/>
+        <VinylDisc/>
+        <VinylDisc/> */}
+      </div>
+
+      <Gramophone/>
       
     </div>
   )
---
+++ src/data/songs.js
@@ -0,0 +1,55 @@
+import paradiseAudio from '../assets/songs/Coldplay   Paradise Instrumental.mp3';
+import paradiseCover from '../assets/VinylImages/paradise1.jpeg'
+
+import dustyAudio from '../assets/songs/DUSTY.mp3'
+import dustyCover from '../assets/VinylImages/dusty.jpeg'
+
+import suit_tieAudio from '../assets/songs/Justin Timberlake - Suit & Tie.mp3'
+import suit_tieCover from '../assets/VinylImages/suit_tie.jpeg'
+
+export const songs = [{
+    id: 1,
+    title: "Paradise" ,
+    audio: paradiseAudio,
+    img: paradiseCover,
+    artist: "Coldplay"
+},
+
+{
+    id: 2,
+    title: "Dusty" ,
+    audio: dustyAudio,
+    img: dustyCover,
+    artist: "Unknown"
+},
+
+{
+    id: 3,
+    title: "Suit & Tie" ,
+    audio: suit_tieAudio,
+    img: suit_tieCover,
+    artist: "Justin Timberlake"
+},
+{
+    id: 4,
+    title: "Paradise" ,
+    audio: paradiseAudio,
+    img: paradiseCover,
+    artist: "Coldplay"
+},
+
+{
+    id: 5,
+    title: "Dusty" ,
+    audio: dustyAudio,
+    img: dustyCover,
+    artist: "Unknown"
+},
+
+{
+    id: 6,
+    title: "Suit & Tie" ,
+    audio: suit_tieAudio,
+    img: suit_tieCover,
+    artist: "Justin Timberlake"
+}];
